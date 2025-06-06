9229c9409f7c703772f46c2fbaa0b24f30939636
Name <meandiligence@gmail.com>
2025-06-06 19:27:43 +0300

    Cleaned up some comments

--- registry/default/hooks/use-persistent-state.ts
+++ registry/default/hooks/use-persistent-state.ts
@@ -37,32 +37,3 @@ export function usePersistentState<T>(key: string, initialValue: T): [T, (value:
   const setValue = (newValue: T) => store.setItem(key, newValue);
   return [value, setValue];
 }
-
-// V 0.1
-
-// import { useState, useEffect } from "react";
-
-// export default function usePersistentState<T>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
-//   const getInitialValue = () => {
-//     if (typeof window === "undefined") return initialValue;
-//     try {
-//       const storedValue = localStorage.getItem(key);
-//       return storedValue ? JSON.parse(storedValue) : initialValue;
-//     } catch (error) {
-//       console.error(`Error reading persistent key "${key}":`, error);
-//       return initialValue;
-//     }
-//   };
-
-//   const [value, setValue] = useState<T>(getInitialValue);
-
-//   useEffect(() => {
-//     try {
-//       localStorage.setItem(key, JSON.stringify(value));
-//     } catch (error) {
-//       console.error(`Error writing persistent key "${key}":`, error);
-//     }
-//   }, [key, value]);
-
-//   return [value, setValue];
-// }
