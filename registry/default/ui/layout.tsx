88d43e0bdc367079efd978e16b905b14a05e5c8a
Primary <meand>
2025-07-14 16:12:51 +0300

    Overhauled the layout component

--- registry/default/ui/layout.tsx
+++ registry/default/ui/layout.tsx
@@ -1,24 +1,28 @@
-import * as React from "react";
-
 import { cn } from "@/lib/utils";
+import React from "react";
+import { type RouteObject, useRoutes } from "react-router-dom";
 
-const Layout = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
+const Root = ({ children }: { children: React.ReactNode }) => <div className="flex flex-col h-screen overflow-hidden">{children}</div>;
+
+const Block = ({ children, className }: { children: React.ReactNode; className?: string }) => <div className={cn("w-full p-4", className)}>{children}</div>;
+
+const Container = ({ children, className }: { children: React.ReactNode; className?: string }) => {
+  return <div className={cn("container [@media(min-width:1920px)]:max-w-[1920px] mx-auto h-full overflow-hidden", className)}>{children}</div>;
+};
+
+const Routes = ({ routes }: { routes: RouteObject[] }) => {
+  const element = useRoutes(routes);
+
   return (
-    <div
-      className={cn(
-        "container flex flex-col h-full gap-2 mx-auto overflow-hidden",
-        className
-      )}
-    >
-      {children}
-    </div>
+    <main className="flex-1 overflow-auto">
+      <div className="min-h-full h-full">{element}</div>
+    </main>
   );
 };
 
-export { Layout };
+export const Layout = {
+  Root,
+  Block,
+  Container,
+  Routes,
+};
